53cf79fa57ee636ba702f35337f47e6561edd1ec
ElizabethCavallo <32904534+ElizabethCavallo@users.noreply.github.com>
2017-12-05 13:04:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -236,35 +236,35 @@ console.log(pollOptions.eon[pollOptionKey]);
 }
 }
 
-//embed
-// function drawChart() {
-//     eon.chart({
-//         pubnub: pb, //same pubnub object, gets data from channel
-//         channel: chan, //same channel
-//         history: true,
-//         generate: {
-//             bindto: '#chart',
-//             data: {
-//                 labels: true,
-//                 type: 'bar',
-//                 colors: {
-//                     'Happy': '#3c3c3c',
-//                     'Mellow': '#f0f0f0',
-//                     'Pumped': '#787878',
-//                     'Romantic': '#b4b4b4'     
-//                 }
-//             },
-//             bar: {
-//                 width: {
-//                     ratio: .75
-//                 }
-//             },
-//             tooltip: {
-//                 show: false //hover over and see chart of counts
-//             }
-//         }
-//     });
-//   }
+embed
+function drawChart() {
+    eon.chart({
+        pubnub: pb, //same pubnub object, gets data from channel
+        channel: chan, //same channel
+        history: true,
+        generate: {
+            bindto: '#chart',
+            data: {
+                labels: true,
+                type: 'bar',
+                colors: {
+                    'Happy': '#3c3c3c',
+                    'Mellow': '#f0f0f0',
+                    'Pumped': '#787878',
+                    'Romantic': '#b4b4b4'     
+                }
+            },
+            bar: {
+                width: {
+                    ratio: .75
+                }
+            },
+            tooltip: {
+                show: false //hover over and see chart of counts
+            }
+        }
+    });
+  }
 
 // drawChart();
 
