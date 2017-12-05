ece61a4c4262a7f7150e367add5a21b53f20e731
ElizabethCavallo <32904534+ElizabethCavallo@users.noreply.github.com>
2017-12-05 12:50:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -268,13 +268,13 @@ console.log(pollOptions.eon[pollOptionKey]);
 
 // drawChart();
 
-function draw() {
-  background(0);
-  fill(255);
-  var level = amplitude.getLevel();
-  var size = map(level, 0, 1, 0, 200);
-  ellipse(width/2, height/2, size, size);
-}
+// function draw() {
+//   background(0);
+//   fill(255);
+//   var level = amplitude.getLevel();
+//   var size = map(level, 0, 1, 0, 200);
+//   ellipse(width/2, height/2, size, size);
+// }
 
 function lightUpRed(){
  var pinR = a.pin(9, 'DIGITAL', 'OUTPUT');
